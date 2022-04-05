fcb2c05cf07689ebcb88f658c2cb68bd71bfa701
bneradt <bneradt@yahooinc.com>
2022-04-05 20:27:58 +0000

    Updating Copyrights to 2022
    These two files were modified this year but haven't had their copyrights
    updated yet.

--- local/include/core/ArgParser.h
+++ local/include/core/ArgParser.h
@@ -1,7 +1,7 @@
 /** @file
  * Powerful and easy-to-use command line parsing for ATS
  *
- * Copyright 2020, Verizon Media
+ * Copyright 2022, Verizon Media
  * SPDX-License-Identifier: Apache-2.0
  */
 
--- local/include/core/http.h
+++ local/include/core/http.h
@@ -1,7 +1,7 @@
 /** @file
  * Common data structures and definitions for Proxy Verifier tools.
  *
- * Copyright 2021, Verizon Media
+ * Copyright 2022, Verizon Media
  * SPDX-License-Identifier: Apache-2.0
  */
 
