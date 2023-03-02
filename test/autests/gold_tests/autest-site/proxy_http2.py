2328b653ee0a3288846dab114bcce179c4f8296f
Zhengxi Li <zhengxi.li@yahooinc.com>
2023-03-02 21:48:55 +0000

    don't send pp if not asked to

--- test/autests/gold_tests/autest-site/proxy_http2.py
+++ test/autests/gold_tests/autest-site/proxy_http2.py
@@ -20,7 +20,7 @@ import threading
 import traceback
 
 from proxy_http1 import ProxyRequestHandler
-from proxy_protocol_context import ProxyProtocolUtil
+from proxy_protocol_context import ProxyProtocolUtil, ProxyProtocolVersion
 
 import eventlet
 from eventlet.green.OpenSSL import SSL, crypto
@@ -49,7 +49,8 @@ class WrapSSSLContext(ssl.SSLContext):
 
     def wrap_socket(self, sock, *args, **kwargs):
         # send proxy protocol header first before TLS handshake
-        ProxyProtocolUtil.send_proxy_header(
+        if ProxyProtocolUtil.pp_version != ProxyProtocolVersion.NONE:
+            ProxyProtocolUtil.send_proxy_header(
             sock, ProxyProtocolUtil.pp_version)
         kwargs['server_hostname'] = self._server_hostname
         return super().wrap_socket(sock, *args, **kwargs)
@@ -324,7 +325,8 @@ class Http2ConnectionManager(object):
 
                     def new_wrap_socket(sock, *args, **kwargs):
                         # send proxy protocol header first before TLS handshake
-                        ProxyProtocolUtil.send_proxy_header(
+                        if ProxyProtocolUtil.pp_version != ProxyProtocolVersion.NONE:
+                            ProxyProtocolUtil.send_proxy_header(
                             sock, ProxyProtocolUtil.pp_version)
                         kwargs['server_hostname'] = self.client_sni
                         return ssl_context.old_wrap_socket(sock, *args, **kwargs)
